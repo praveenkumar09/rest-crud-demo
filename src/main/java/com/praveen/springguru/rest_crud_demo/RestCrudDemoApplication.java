149a6a59e1cb8ce2db269cccee5798d90747b829
praveenkumar09 <tpkprav@gmail.com>
2024-08-04 21:21:06 +0800

    Inital commit

---
+++ src/main/java/com/praveen/springguru/rest_crud_demo/RestCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.springguru.rest_crud_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/springguru/rest_crud_demo/RestCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.springguru.rest_crud_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
