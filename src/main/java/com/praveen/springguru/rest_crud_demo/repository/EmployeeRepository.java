d9c79e14dac82197690da3f97debb16b1911717e
praveenkumar09 <tpkprav@gmail.com>
2024-08-08 22:20:15 +0800

    sprint data rest api's added

--- src/main/java/com/praveen/springguru/rest_crud_demo/repository/EmployeeRepository.java
+++ src/main/java/com/praveen/springguru/rest_crud_demo/repository/EmployeeRepository.java
@@ -3,8 +3,9 @@ package com.praveen.springguru.rest_crud_demo.repository;
 
 import com.praveen.springguru.rest_crud_demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(path = "/employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
