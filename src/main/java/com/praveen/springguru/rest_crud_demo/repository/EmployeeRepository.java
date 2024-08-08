a2596d50113d6364a4973464b83bbc06f9238278
praveenkumar09 <tpkprav@gmail.com>
2024-08-08 22:28:56 +0800

    Added properties for spring data rest

--- src/main/java/com/praveen/springguru/rest_crud_demo/repository/EmployeeRepository.java
+++ src/main/java/com/praveen/springguru/rest_crud_demo/repository/EmployeeRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(path = "/employees")
+@RepositoryRestResource(path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
